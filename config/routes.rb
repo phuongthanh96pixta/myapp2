fa5c22e41e194b913e1cb69a31fdb5472e4acd3d
phuongthanh96pixta <thanh.phamphuong@pixta.co.jp>
2018-07-25 15:30:40 +0700

    update static_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  
+  root to: 'static_pages#home'
   get 'static_pages/help'
   #devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#home'
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   #devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
     confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
